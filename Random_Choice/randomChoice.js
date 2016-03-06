97b48e7ba07e59cb916a782489f4fba92590bd77
BlueAlder <sam_calamos@hotmail.com>
2016-03-06 22:38:23 +1100

    Added a Random Selection Thingo

---
+++ Random_Choice/randomChoice.js
@@ -0,0 +1,27 @@
+function select()
+{
+	var first = document.getElementById("first").value;
+	var second = document.getElementById("second").value;
+
+	var select = Math.round(Math.random());
+
+
+	if (first =="" || second == "")
+	{
+		document.getElementById("result").innerHTML = "You should: Put something in the boxes ;)";
+		
+	}
+
+	else{
+		if (select == 0){
+		document.getElementById("result").innerHTML = "You should: "+first;
+		}
+
+		else if (select == 1 )
+		{
+			document.getElementById("result").innerHTML = "You should: "+second;
+		}
+	}
+
+	
+}
